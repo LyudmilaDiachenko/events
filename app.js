b4e185c6ad2c11eb92904e739b80ac9bc9b9d7e4
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-05-17 15:51:13 +0300

    rename routes to controllers

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
+app.use('/', require('./controllers/index'));
 
 app.use(function(req, res, next) {
   next(createError(404));
