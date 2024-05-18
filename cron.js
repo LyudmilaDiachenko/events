91118dd3295bcba941eb933cb3a957e8340f7aa2
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-05-18 12:43:32 +0300

    fill db cron

--- cron.js
+++ cron.js
@@ -1,3 +1,6 @@
+const redis = require('redis');
+const request = require('request');
+
 const db = redis.createClient({
     password: 'sYjjTQwUgBvcqCiWX7UsqlLE6QF5v53f',
     socket: {
@@ -6,17 +9,36 @@ const db = redis.createClient({
     }
 })
 db.connect();
+db.flushAll()
 
-API.getAll()
-.then(list => 
-    list.map(
-        event => db.hSet(
-            `events:${event.title}`, 
-            {
-                title: event.title, 
-                description: event.description, 
-                date: event.date
+function parseDB(page = 1){
+    console.debug('Start page: ', page)
+    request({
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&include_adult=false&language=uk-UA&year=2025&page=${page}`,
+        json: true
+    }, (err, resp, data) => {
+        // console.debug(data)
+        data.results.map(
+            event => {
+                // console.debug(event)
+                db.hSet(
+                    `events:${event.id}`, 
+                    {
+                        id: event.id,
+                        title: event.title, 
+                        // description: event.overview, 
+                        // date: event.release_date,
+                        // image: event.poster_path,
+                        // background: event.backdrop_path,
+                        // organizer: 
+                        // https://image.tmdb.org/t/p/w600_and_h900_bestv2/
+                    }
+                )
             }
         )
-    )
-)
+        console.debug('Finished page: ', data.page)
+    })
+}
+for(let page = 1; page <= 10; page++){
+    parseDB(page)
+}
